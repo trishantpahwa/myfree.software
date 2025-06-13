83c0f7e82a430c6f7323dd960c6cdfeff515e384
Trishant Pahwa <trishantpahwa@gmail.com>
2025-06-13 20:20:11 +0530

    Add GitHub OAuth login functionality and Lottie animation support (#4)

---
+++ src/pages/login.tsx
@@ -0,0 +1,47 @@
+import { DotLottieReact } from '@lottiefiles/dotlottie-react';
+import { useRouter } from 'next/router';
+import { useEffect } from 'react';
+
+export default function Login() {
+
+    const router = useRouter();
+    const { code } = router.query;
+
+    useEffect(() => {
+        const _login = async () => {
+            const response = await fetch("/api/auth/login", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify({ code }),
+            });
+            if (response.ok) {
+                const data = await response.json();
+                localStorage.setItem("token", data.token);
+                router.push("/");
+            } else {
+                console.error("Login failed");
+            }
+        };
+        if (code) _login();
+    }, [code])
+
+    const login = () => {
+        router.push(`https://github.com/login/oauth/authorize?client_id=${process.env.NEXT_PUBLIC_GITHUB_CLIENT_ID}&scope=repo,user:email`);
+    };
+
+    return (
+        <>
+            <div className="h-screen flex items-center justify-center">
+                <DotLottieReact
+                    src="/github.lottie"
+                    loop
+                    autoplay
+                    className="w-1/6 h-1/6 cursor-pointer"
+                    onClick={login}
+                />
+            </div>
+        </>
+    );
+};
